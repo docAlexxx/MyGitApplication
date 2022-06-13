89c4f27c6bd2719594efba884052ab9c2b22c65c
docAlexxx <docalex@mail.ru>
2022-06-13 18:12:01 +0400

    lesson1 done

--- app/src/main/java/com/example/mygitapplication/ui/users/UserViewHolder.kt
+++ app/src/main/java/com/example/mygitapplication/ui/users/UserViewHolder.kt
@@ -5,8 +5,8 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.example.mygitapplication.R
-import com.example.mygitapplication.model.User
 import com.example.mygitapplication.databinding.UserItemBinding
+import com.example.mygitapplication.model.User
 
 class UserViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
     LayoutInflater.from(parent.context).inflate(R.layout.user_item, parent, false)
@@ -17,7 +17,7 @@ class UserViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
         binding.avatarImageView.load(userEntity.avatarUrl)
         binding.loginTextView.text = userEntity.login
         binding.idTextView.text = userEntity.id.toString()
-        binding.typeTextView.text=userEntity.type
+        binding.typeTextView.text = userEntity.type
     }
 
 }
